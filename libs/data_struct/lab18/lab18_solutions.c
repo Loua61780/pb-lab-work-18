f61a37db308b18e2007ef1c868215a3a29ef188e
Loua61780 <160328520+Loua61780@users.noreply.github.com>
2024-05-11 12:37:43 +0300

    solved task #1

--- libs/data_struct/lab18/lab18_solutions.c
+++ libs/data_struct/lab18/lab18_solutions.c
@@ -1,1 +1,17 @@
 #include "lab18_solutions.h"
+#include "../string/string.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void removeExtraSpaces(char* source) {
+    char* writePointer = source;
+    while (*source != '\0') {
+        if (*source != *(source + 1)) {
+            *writePointer = *source;
+            writePointer++;
+        }
+        source++;
+    }
+    *writePointer = '\0';
+}
